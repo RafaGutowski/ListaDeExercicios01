8fd58836f036efb46c448d6b05facd15daf8ba7c
RafaGutowski <162761689+RafaGutowski@users.noreply.github.com>
2024-04-16 10:09:57 -0300

    Update Exercício 5

--- Exercicio5.java
+++ Exercicio5.java
@@ -1,9 +1,16 @@
 
 public class Exercicio5 {
     
-    public static void executar(){
-
-
-
-    }
+        public static void executar() {
+            int A = Prompt.lerInteiro("Digite o primeiro número: ");
+            int B = Prompt.lerInteiro("Digite o segundo número: ");
+    
+            System.out.printf("Valor original das variáveis: A = %d e B = %d" , A , B);
+    
+            int aux = A;
+            A = B;
+            B = aux;
+    
+            System.out.printf("\nValor trocado: A = %d e B = %d" , A , B);
+        }
 }
